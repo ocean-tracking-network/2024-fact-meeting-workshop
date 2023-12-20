d3b79c8aa06c8d48fc208fcffb2a79ad483c72d9
Bruce Delo <bruce.delo@gmail.com>
2023-12-20 10:47:38 -0400

    Removed unnecessary packages.

--- setup/setup.R
+++ setup/setup.R
@@ -5,19 +5,31 @@
 
 # Advanced Telemetry Workshop ONLY: You must install also GDAL software, which can take a long time. See the extra setup document provided.
 
+#Due to recent issues with certain spatial packages being delisted from CRAN, you will need to install the archived version of rgeos before the package will work. 
+#You can get the most recent archive from https://cran.r-project.org/src/contrib/Archive/rgeos/. 
+#At time of writing, 0.6-4 is the most recent. 
+#You will then need to install the rgeos package. Make sure you change the path in the first line of the script.
+
 # Once R/RStudio is installed: open RStudio and run this install script. Please run it line-by-line instead of all at once in case there are errors.
 
 #Note: When running through the installs, you may encounter a prompt asking you to upgrade dependent packages.
       #Choosing Option `3: None`, works in most situations and will prevent upgrades of packages you weren't explicitly looking to upgrade.
 
+# Install the archived rgeos package: 
+install.packages("YOUR/PATH/TO/rgeos_0.6-4.tar.gz", repos = NULL, type = "source")
 
-
-## Beginner R Workshop Requirements ----
-# Tidyverse (data cleaning and arrangement)
+#Tidyverse (data leaning and arrangement)
 install.packages('tidyverse')
+install.packages('dplyr')
 
 # Lubridate - part of Tidyverse, improves the process of creating date objects
 install.packages('lubridate')
+
+#ggplot2 - common and well-supported package for data visualisation
+install.packages('ggplot2')
+
+# ReadXL - reads Excel format
+install.packages('readxl')
 
 # GGmap - complimentary to ggplot2, which is in the Tidyverse
 install.packages('ggmap')
@@ -25,80 +37,15 @@ install.packages('ggmap')
 # Plotly - Interactive web-based data visualization
 install.packages('plotly')
 
-# ReadXL - reads Excel format
-install.packages("readxl")
-
 # Viridis - color scales in this package are easier to read by those with colorblindness, and print well in grey scale.
-install.packages("viridis")
-
-
-
-
-## Advanced Telemetry Workshop Requirements ----
-# rgdal
-install.packages('rgdal')
-library(rgdal)
-rgdal::getGDALVersionInfo()
-
-# Tidyverse (data cleaning and arrangement)
-install.packages('tidyverse')
-
-# glatos - acoustic telemetry package that does filtering, vis, array simulation, etc.
-install.packages('remotes')
-library(remotes) 
-remotes::install_github('ocean-tracking-network/glatos')
-
-#Additional packages for mapping.
-install.packages('stringr')
-install.packages('mapview')
-install.packages('spdplyr')
-
-# Lubridate - part of Tidyverse, improves the process of creating date objects
-install.packages('lubridate')
-
-# GGmap - complimentary to ggplot2, which is in the Tidyverse
-install.packages('ggmap')
-
-#SP and Raster packages for mapping.
-install.packages('sp')
-install.packages('raster')
-
-# Install actel
-library(remotes)
-remotes::install_github("hugomflavio/actel", 
-                        build_opts = c("--no-resave-data", "--no-manual"), build_vignettes = TRUE)
-
-# Install packages for building/displaying R Markdown
-install.packages('rmarkdown')
-install.packages('knitr', dependencies = TRUE)
-
-# Install additonal packages for `remora` lesson
-install.packages('readr')
-install.packages('sf')
-install.packages('stars')
-
-# Install remora
-install.packages('devtools')
-library(devtools)
-devtools::install_github('ocean-tracking-network/remora@workshop_ready', force=TRUE)
-
-# Install packages for animating detection data
-install.packages('remotes')
-library(remotes) 
-remotes::install_github("jmlondon/pathroutr")
-
-install.packages('plotly')
-install.packages('gganimate')
-install.packages('ggspatial')
-                                                            
-                                                            
+install.packages('viridis')                            
 
 ### Dataset and Code -----
-# Once the packages are installed, you can download the datasets and code for this workshop from https://github.com/ocean-tracking-network/otn-workshop-base/tree/master.
+# Once the packages are installed, you can download the datasets and code for this workshop from https://github.com/ocean-tracking-network/2024-fact-meeting-workshop/tree/master.
 # 1) Select the GREEN "code" button at the top and choose "Download ZIP"
 # 2) Unzip the folder and move to secure location on your computer (Documents, Desktop etc.)
 # 3) Copy the folder's path and use it to set your working directly in R using `setwd('<path-to-folder>')`.
 
 # If you are familiar with Git and Github, feel free to clone this repository as you normally would,
-# by running `git clone https://github.com/ocean-tracking-network/otn-workshop-base/tree/master.git` in a terminal program
+# by running `git clone https://github.com/ocean-tracking-network/2024-fact-meeting-workshop.git` in a terminal program
 # and following from step 3 above.
